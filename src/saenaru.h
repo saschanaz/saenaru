669a4b38ab30b14233ca77f71e2eafcb9cbed5ed
Won Kyu Park <wkpark@gmail.com>
2010-03-22 12:38:23 +0000

    [#315232] support a fake cursor with the word editing mode.

--- src/saenaru.h
+++ src/saenaru.h
@@ -27,7 +27,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: saenaru.h,v 1.33 2010/03/16 15:28:28 wkpark Exp $
+ * $Id: saenaru.h,v 1.34 2010/03/19 08:47:42 wkpark Exp $
  */
 
 #include <indicml.h>
@@ -143,6 +143,7 @@ typedef TCHAR             MYCHAR;
 #define WM_UI_CANDMOVE    (WM_USER+603)
 #define WM_UI_GUIDEMOVE   (WM_USER+604)
 #define WM_UI_STATEHIDE   (WM_USER+605)
+#define WM_UI_CARET       (WM_USER+606)
 
 /* Escape Functions */
 #define IME_ESC_PRI_GETDWORDTEST (IME_ESC_PRIVATE_FIRST + 0)
@@ -489,6 +490,8 @@ void PASCAL CreateCompWindow(HWND hUIWnd, LPUIEXTRA lpUIExtra,LPINPUTCONTEXT lpI
 void PASCAL MoveCompWindow(LPUIEXTRA lpUIExtra,LPINPUTCONTEXT lpIMC);
 void PASCAL HideCompWindow(LPUIEXTRA lpUIExtra);
 void PASCAL SetFontCompWindow(LPUIEXTRA lpUIExtra);
+
+int PASCAL DrawCompCaret(HWND hCompWnd);
 
 /* uisoft.c      */
 void PASCAL ShowSoftKbd(HWND, int);
